1a786673346c1f6623632eba89da94b25537d5b5
Suresh-CS <Suresh.bscsses21@iba-suk.edu.pk>
2024-05-06 00:47:45 +0500

    Done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import './App.css';
 import React, { useState } from 'react';
 import ToDoList from './ToDolist';
@@ -6,7 +5,7 @@ import ToDoList from './ToDolist';
 function App(props) {
 
   const [item, updateitem] = useState("");
-  const [item1, updateitem1] = useState([]);//this indicate initial value given to var is null array
+  const [item1, updateitem1] = useState([]);
   
 
   const events = (obj) => {
@@ -14,20 +13,19 @@ function App(props) {
   }
 
   const additem = (obj) => {
-    updateitem1((olditem) => { //using operator for saving total value of item value at once in item1 array
+    updateitem1((olditem) => {
 
         return [...olditem, item]
 
     });
-    updateitem("");//this will make item value to empty whenever we add an item by click button
+    updateitem("");
   }
 
-  //item deleting function
   const remove = (id) =>{
     console.log("deleted");
     updateitem1((olditem) => { 
-      return olditem.filter( (olditem,index)=>{ //it check which id is deleted and return filter array
-        return index!==id;//it return index that is not equal to id than return that index array only
+      return olditem.filter( (olditem,index)=>{ 
+        return index!==id;
       } )
     });
   }
@@ -36,15 +34,14 @@ function App(props) {
     <>
       <div className="main_div">
         <div className="center_div">
-          <span><h1>ToDo List</h1></span>
+          <span><h1>To Do List</h1></span>
           <div className="inner_div">
             <input className="inp" type="text" placeholder="Add an item" onChange={events} value={item}></input>
             <button id="btn1" onClick={additem}> + </button>
           </div>
           <br /><br />
           <ul>
-            {/* <li>{item1[0]}</li>
-                <li>{item1[1]}</li>     instead of thi we can use map method for array*/
+            {
 
               item1.map(function (itemvalue, index) {
                 return (<ToDoList
@@ -66,5 +63,3 @@ function App(props) {
 
 export default App;
 
-/*In above at first updateitem is setting updated input value to item and updateitem1 then get that updated
-item value and send it to item1 which is then render in list*/
--- src/ToDolist.jsx
+++ src/ToDolist.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 
 const ToDoList = (props) => {
-
-
-    
+   
     return (
         <>
             <li>
